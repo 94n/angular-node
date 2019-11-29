bf6f6955632f727b33c6235bcb8f8ffc19671f50
makstitoff <makstitoff@gmail.com>
2019-11-29 14:26:33 +0200

    table styles

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,8 +2,14 @@
 
 angular.module('svrTestTaskApp')
   .controller('MainController', function ($scope, $http, $modal) {
-    console.log(".controller('MainController', function ($scope, $http, $modal) {");
     $http.get('/api/things').then(function (response) {
-      $scope.client_data = response;
+      $scope.parseDate = function (date) {
+        return new Date(Date.parse(date));
+      };
+      $scope.expandSelected = function (cap_expanded) {
+        $scope.cap_expanded = !cap_expanded;
+      };
+      $scope.cap_expanded = false;
+      $scope.data = JSON.parse(response.data);
     });
   });
